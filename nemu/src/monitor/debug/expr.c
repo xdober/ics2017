23eb29040d7b4d164f055001bc660e034df5e855
tracer-ics2017 <tracer@njuics.org>
2017-09-09 15:38:38 +0000

    >  compile U201414803 ics Linux 803cd871e9ec 4.9.0-deepin10-amd64 #1 SMP PREEMPT Deepin 4.9.40-1 (2017-07-28) x86_64 GNU/Linux  15:38:38 up 53 min,  3 users,  load average: 0.41, 0.28, 0.28

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -92,12 +92,12 @@ static bool make_token(char *e) {
 
 
             switch (rules[i].token_type) {
-                case '+': tokens[i].type = '+'; break;
-                case '-': tokens[i].type = '-'; break;
-                case '*': tokens[i].type = '*'; break;
-                case '/': tokens[i].type = '/'; break;
-                case ')': tokens[i].type = ')'; break;
-                case '(': tokens[i].type = '('; break;
+                case '+': tokens[nr_token].type = '+'; break;
+                case '-': tokens[nr_token].type = '-'; break;
+                case '*': tokens[nr_token].type = '*'; break;
+                case '/': tokens[nr_token].type = '/'; break;
+                case ')': tokens[nr_token].type = ')'; break;
+                case '(': tokens[nr_token].type = '('; break;
                 case TK_INT: 
                           tokens[i].type = TK_INT; 
                           strcpy(tokens[i].str, rules[i].regex);
@@ -203,7 +203,7 @@ int findOP(int p, int q){
     int pos=0;
     printf("operaters: ");
     for (i = p; i <= q; ++i) {
-        if (isOP(i)/* && !inBK(i, p, q)*/ ) {
+        if (isOP(i) && !inBK(i, p, q) ) {
             roots[pos]=i;
             printf("%d,%c\t",i,tokens[i].type);
             i++;

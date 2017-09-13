fdc95d2ef41ff43105a219d22cb4eae58a056603
tracer-ics2017 <tracer@njuics.org>
2017-09-13 03:01:56 +0000

    >  compile U201414803 ics Linux 803cd871e9ec 4.9.0-deepin10-amd64 #1 SMP PREEMPT Deepin 4.9.40-1 (2017-07-28) x86_64 GNU/Linux  03:01:56 up  3:00,  6 users,  load average: 0.65, 0.45, 0.34 c1ed6dbed29a1edec9085c70e5bd8616b4752a5f

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -27,6 +27,7 @@ make_EHelper(call) {
   // the target address is calculated at the decode stage
 //  TODO();
   decoding.jmp_eip = decoding.seq_eip+id_dest->val;
+  decoding.is_jmp = 1;
   print_asm("call %x", decoding.jmp_eip);
 }
 

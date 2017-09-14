b599ff8d8c84e941ab2050deaa61ba142c7d6ea9
tracer-ics2017 <tracer@njuics.org>
2017-09-14 11:48:18 +0000

    >  compile U201414803 ics Linux 803cd871e9ec 4.9.0-deepin10-amd64 #1 SMP PREEMPT Deepin 4.9.40-1 (2017-07-28) x86_64 GNU/Linux  11:48:18 up 10:12,  7 users,  load average: 0.62, 0.54, 0.51 748f79ba3aa2469d3b3aa038f96cd7035fcca026

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -9,7 +9,7 @@ make_EHelper(push) {
 //  TODO();
 //  operand_write(id_dest, &id_src->val);
   rtl_push(&id_src->val);
-  printf("pushed: 0x%x\n", id_src->val);
+  printf("id_src: 0x%x\n", id_src->val);
   print_asm_template1(push);
 }
 

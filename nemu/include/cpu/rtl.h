db8b638c29c3f8f7d6dc4069c25b334b5cdea28c
U201414803 -Xu Rongren <xuduo1017@live.com>
2017-09-15 03:50:06 +0000

    start pa2.2

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -208,6 +208,8 @@ static inline void rtl_update_SF(const rtlreg_t* result, int width) {
   }
 }
 
+static inline void rtl_sete()
+
 static inline void rtl_update_ZFSF(const rtlreg_t* result, int width) {
   rtl_update_ZF(result, width);
   rtl_update_SF(result, width);
--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -80,7 +80,10 @@ make_EHelper(not) {
 }
 
 make_EHelper(sete) {
-  TODO();
+ // TODO();
+  uint8_t subcode = decoding.opcode & 0xf;
+  rtl_sete(&t2, subcode);
+  operand_write(id_dest, &t2);
 
-  print_asm_template1(sete);
+  print_asm("set%s %s", get_cc_name(subcode), id_dest->str);
 }

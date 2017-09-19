c2cb6a29871c28b366e8aaccf07e5759fb300646
tracer-ics2017 <tracer@njuics.org>
2017-09-19 00:24:02 +0000

    >  compile U201414803 ics Linux 803cd871e9ec 4.9.0-deepin10-amd64 #1 SMP PREEMPT Deepin 4.9.40-1 (2017-07-28) x86_64 GNU/Linux  00:24:02 up 17 min,  5 users,  load average: 0.41, 0.36, 0.34 7c2aa1349f46814a27d080e115cb621d5cf4d938

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -208,7 +208,9 @@ static inline void rtl_update_SF(const rtlreg_t* result, int width) {
   }
 }
 
-static inline void rtl_sete()
+static inline void rtl_sete(){
+
+}
 
 static inline void rtl_update_ZFSF(const rtlreg_t* result, int width) {
   rtl_update_ZF(result, width);

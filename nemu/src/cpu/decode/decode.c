66678bff055a12d5ef571f1c22f9741059ac131e
tracer-ics2017 <tracer@njuics.org>
2017-09-20 00:34:26 +0000

    >  compile U201414803 ics Linux 803cd871e9ec 4.9.0-deepin10-amd64 #1 SMP PREEMPT Deepin 4.9.40-1 (2017-07-28) x86_64 GNU/Linux  00:34:26 up 35 min,  7 users,  load average: 0.33, 0.44, 0.34 5cd5c4dce698cea133b9a5cc2666bca7f71d486

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -273,6 +273,9 @@ make_DHelper(a2O) {
 make_DHelper(J) {
   decode_op_SI(eip, id_dest, false);
   // the target address can be computed in the decode stage
+  if(id_dest->width==1 && (id_dest->simm & 0x80)){
+      id_dest->simm=id_dest->val | 0xffffff00;
+  }
   decoding.jmp_eip = id_dest->simm + *eip;
 }
 

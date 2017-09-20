fbed2c9cff3b595ce22e021672b79c726ac081cf
tracer-ics2017 <tracer@njuics.org>
2017-09-20 00:55:48 +0000

    >  compile U201414803 ics Linux 803cd871e9ec 4.9.0-deepin10-amd64 #1 SMP PREEMPT Deepin 4.9.40-1 (2017-07-28) x86_64 GNU/Linux  00:55:48 up 56 min,  7 users,  load average: 0.06, 0.11, 0.17 ccb972a119713a4c1f14abb118dc3023671eeaa9

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -48,12 +48,31 @@ make_EHelper(inc) {
   t1=1;
   rtl_add(&t2, &id_dest->val, &t1);
   operand_write(id_dest, &t2);
+
+  rtl_update_ZFSF(&t2, id_dest->width);
+
+  rtl_sltu(&t0, &t2, &id_dest->val);
+  rtl_set_CF(&t0);
+
+  rtl_xor(&t0, &id_dest->val, &id_src->val);
+  rtl_not(&t0);
+  rtl_xor(&t1, &id_dest->val, &t2);
+  rtl_and(&t0, &t0, &t1);
+  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_set_OF(&t0);
   print_asm_template1(inc);
 }
 
 make_EHelper(dec) {
-  TODO();
+//  TODO();
+  t1=1;
+  rtl_sub(&t2, &id_dest->val, &t1);
+  operand_write(id_dest, &t2);
 
+  rtl_update_ZFSF(&t2, id_dest->width);
+  
+  rtl_sltu(&t0, &t2, &id_dest->val);
+  rtl_set_CF(&t0);
   print_asm_template1(dec);
 }
 

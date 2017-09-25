cb917ad03d29994d3dcad1e92ba55ee238225a79
tracer-ics2017 <tracer@njuics.org>
2017-09-25 01:17:32 +0000

    >  compile U201414803 ics Linux 803cd871e9ec 4.9.0-deepin10-amd64 #1 SMP PREEMPT Deepin 4.9.40-1 (2017-07-28) x86_64 GNU/Linux  01:17:32 up  1:18,  6 users,  load average: 0.40, 0.50, 0.44 816202298060f3bab9cf2b4c7702b7ebfe4bfc4d

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -46,7 +46,7 @@ void pio_write(ioaddr_t, int, uint32_t);
 
 make_EHelper(in) {
  // TODO();
-  t2=vaddr_read(id_src->val, id_dest->width);
+  t2=pio_read(id_src->val, id_dest->width);
   operand_write(id_dest, &t2);
   print_asm_template2(in);
 
@@ -57,7 +57,7 @@ make_EHelper(in) {
 
 make_EHelper(out) {
   //TODO();
-  vaddr_write(id_dest->val, id_src->width, id_src->val);
+  pio_write(id_dest->val, id_src->width, id_src->val);
   print_asm_template2(out);
 
 #ifdef DIFF_TEST

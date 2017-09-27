d5ccfa0fa8cc607a7671fe2638bbf5437d55d2f7
U201414803 -Xu Rongren <xuduo1017@live.com>
2017-09-27 03:27:43 +0000

    pass keyboard test.

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -1,6 +1,8 @@
 #include <am.h>
 #include <x86.h>
-
+#define I8042_DATA_PORT 0x60
+#define I8042_STATUS_PORT 0x64
+#define I8042_STATUS_HASKEY_MASK 0x1
 #define RTC_PORT 0x48   // Note that this is not standard
 static unsigned long boot_time;
 
@@ -32,5 +34,7 @@ void _draw_sync() {
 }
 
 int _read_key() {
-  return _KEY_NONE;
+    int key;
+    key = (I8042_STATUS_HASKEY_MASK&inb(I8042_STATUS_PORT))?inl(I8042_DATA_PORT):_KEY_NONE;
+  return key;
 }

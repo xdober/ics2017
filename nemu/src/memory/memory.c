1c10a322537196126ed1ca039072774ae73f66e9
tracer-ics2017 <tracer@njuics.org>
2017-09-27 03:44:52 +0000

    >  compile U201414803 ics Linux 803cd871e9ec 4.9.0-deepin10-amd64 #1 SMP PREEMPT Deepin 4.9.40-1 (2017-07-28) x86_64 GNU/Linux  03:44:52 up  3:47,  7 users,  load average: 0.46, 0.51, 0.81 c5035f3d1f172a992bf0e3813938780f2c0fc208

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,5 +1,5 @@
 #include "nemu.h"
-
+#include "../../include/device/mmio.h"
 #define PMEM_SIZE (128 * 1024 * 1024)
 
 #define pmem_rw(addr, type) *(type *)({\
@@ -12,11 +12,18 @@ uint8_t pmem[PMEM_SIZE];
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
-  return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+  int port_video = is_mmio(addr);
+  return port_video==-1? pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3)):mmio_read(addr, len, port_video);
 }
 
 void paddr_write(paddr_t addr, int len, uint32_t data) {
-  memcpy(guest_to_host(addr), &data, len);
+    int port_video = is_mmio(addr);
+    if (port_video==-1) {  
+        memcpy(guest_to_host(addr), &data, len);
+    }
+    else {
+        mmio_write(addr, len, data, port_video);
+    }
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {

6782b99e05859cf63de9988168a5b0e958381a68
tracer-ics2017 <tracer@njuics.org>
2017-09-27 01:32:36 +0000

    >  compile U201414803 ics Linux 803cd871e9ec 4.9.0-deepin10-amd64 #1 SMP PREEMPT Deepin 4.9.40-1 (2017-07-28) x86_64 GNU/Linux  01:32:36 up  1:34,  7 users,  load average: 0.23, 0.45, 0.77 405e46bc727a3f71a49a441aeb3cec5b711b2a95

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -41,8 +41,8 @@ static make_EHelper(name) { \
 
 /* 0x80, 0x81, 0x83 */
 make_group(gp1,
-    EXW(add, 1), EXW(or, 1), EMPTY, EXW(sbb, 1),
-    EXW(and, 1), EXW(sub,1), EMPTY, EXW(cmp, 1))
+    EXW(add, 1), EXW(or, 1), EXW(adc, 1), EXW(sbb, 1),
+    EXW(and, 1), EXW(sub,1), EXW(xor, 1), EXW(cmp, 1))
 
   /* 0xc0, 0xc1, 0xd0, 0xd1, 0xd2, 0xd3 */
 make_group(gp2,
@@ -61,7 +61,7 @@ make_group(gp4,
 
   /* 0xff */
 make_group(gp5,
-    EX(inc), EMPTY, EX(call_rm), EMPTY,
+    EX(inc), EX(dec), EX(call_rm), EMPTY,
     EX(jmp_rm), EMPTY, EXW(push,1), EMPTY)
 
   /* 0x0f 0x01*/
